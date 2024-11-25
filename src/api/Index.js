987bf84c00457eb476f5050473eab43ddb39b20e
Mangesh Namdeo Gosavi <148337560+Mangesh-Gosavi@users.noreply.github.com>
2024-11-25 15:38:50 +0530

    Update Index.js

--- src/api/Index.js
+++ src/api/Index.js
@@ -1,13 +1,11 @@
-const baseUrl = "https://api.weatherapi.com/v1/current.json?key=644f5640c66e496cae445300241407"
+const baseURL = "https://api.weatherapi.com/v1/current.json?key=644f5640c66e496cae445300241407"
 
-export const getWeatherDataForCity = async(city) =>{
-
-    const response = await fetch(`${baseUrl}&q=${city}&aqi=yes`);
+export const getWeatherDataForCity = async (city) => {
+    const response = await fetch(`${baseURL}&q=${city}&aqi=yes`);
     return await response.json();
-};
-
-export const getWeatherDataForLocation = async(lat,long) =>{
-
-    const response = await fetch(`${baseUrl}&q=${lat},&q=${long}&aqi=yes`);
+  };
+  
+  export const getWeatherDataForLocation = async (lat, lon) => {
+    const response = await fetch(`${baseURL}&q=${lat},${lon}&aqi=yes`);
     return await response.json();
-};
+  };
