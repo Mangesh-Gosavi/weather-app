153c394f616d1c943b206ded6c8ce23aea79c321
Mangesh Namdeo Gosavi <148337560+Mangesh-Gosavi@users.noreply.github.com>
2024-11-25 14:38:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
         <div class='min-h-full  flex flex-col justify-center items-center relative z-20'>
           <form class="w-4/5 flex flex-col gap-7 items-center bg-white rounded-xl 
           xl:text-2xl 2xl-4xl shadow shadow-slate-700">
-            <h1 class="mt-7 font-serif font-semibold italic bg-gradient-to-r from-red-600 via-green-700 to-indigo-700 inline-block text-transparent bg-clip-text">Weather Forecast</h1>
+            <h1 class="mt-7 font-serif font-semibold italic">Weather Forecast</h1>
             <Input />
             <Button onClick={weather.fetchData} value="Search" />
             <Card />
